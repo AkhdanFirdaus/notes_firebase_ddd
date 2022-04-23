8d1826e53217e99895bfdaf0c49a97fd15950979
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-04-23 22:00:42 +0700

    Update IAuthFacade using Either

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,13 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/auth/auth_failure.dart';
 import 'package:ddd_notes/domain/core/value_objects.dart';
 
 abstract class IAuthFacade {
-  Future<void> registerWithEmailPassword({
+  Future<Either<AuthFailure, Unit>> registerWithEmailPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
-  Future<void> signInWithEmailPassword({
+  Future<Either<AuthFailure, Unit>> signInWithEmailPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
-  Future<void> signInWithGoogle();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
