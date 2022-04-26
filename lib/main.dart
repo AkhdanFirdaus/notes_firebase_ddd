e164fd43ee5d8ed2cfd3c9f80f5c1a14d2eb07c9
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-04-26 20:46:56 +0700

    Sign in form ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'package:ddd_notes/injection.dart';
+import 'package:ddd_notes/presentation/core/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
   configureInjection(Environment.prod);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(child: Text("Hello World")),
-      ),
-    );
-  }
+  runApp(const AppWidget());
 }
---
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -0,0 +1,143 @@
+import 'package:ddd_notes/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SignInFormBloc, SignInFormState>(
+      listener: (context, state) {
+        state.authFailureOrSuccessOption.fold(
+          () => {},
+          (either) => either.fold(
+            (l) => {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text(
+                    l.map(
+                      cancelledByUser: (_) => 'Cancelled',
+                      serverError: (_) => 'Server Error',
+                      emailAlreadyInUse: (_) => 'Email already in use',
+                      invalidEmailAndPasswordCombination: (_) =>
+                          'Invalid email an password combination',
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            },
+            (r) => {},
+          ),
+        );
+      },
+      builder: (context, state) {
+        return Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: ListView(
+            padding: const EdgeInsets.all(24),
+            children: [
+              const Text(
+                '📝',
+                style: TextStyle(fontSize: 130),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  labelText: 'Email',
+                ),
+                autocorrect: false,
+                onChanged: (value) {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(SignInFormEvent.emailChanged(value));
+                },
+                validator: (_) {
+                  return context
+                      .read<SignInFormBloc>()
+                      .state
+                      .emailAddress
+                      .value
+                      .fold(
+                        (l) => l.maybeMap(
+                          invalidEmail: (_) => 'Invalid Email',
+                          orElse: () => null,
+                        ),
+                        (r) => null,
+                      );
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  labelText: 'Password',
+                ),
+                autocorrect: false,
+                obscureText: true,
+                onChanged: (value) {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(SignInFormEvent.passwordChanged(value));
+                },
+                validator: (_) {
+                  return context
+                      .read<SignInFormBloc>()
+                      .state
+                      .password
+                      .value
+                      .fold(
+                        (l) => l.maybeMap(
+                          shortPassword: (_) => 'Invalid Email',
+                          orElse: () => null,
+                        ),
+                        (r) => null,
+                      );
+                },
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        context.read<SignInFormBloc>().add(const SignInFormEvent
+                            .signInWithEmailAndPasswordPressed());
+                      },
+                      child: const Text('SIGN IN'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        context.read<SignInFormBloc>().add(const SignInFormEvent
+                            .registerWithEmailAndPasswordPressed());
+                      },
+                      child: const Text('REGISTER'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              FractionallySizedBox(
+                widthFactor: 1,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context
+                        .read<SignInFormBloc>()
+                        .add(const SignInFormEvent.signInWithGooglePressed());
+                  },
+                  child: const Text("SIGN IN WITH GOOGLE"),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
