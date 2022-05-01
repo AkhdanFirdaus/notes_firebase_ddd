c9e56eee09ecf57be7e3cc9052b23cbd2c5b8f93
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-01 22:46:05 +0700

    Note Entities

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -21,6 +21,13 @@ abstract class ValueObject<T> {
     );
   }
 
+  Either<ValueFailure<dynamic>, Unit> get failureOrUnit {
+    return value.fold(
+      (l) => left(l),
+      (r) => right(unit),
+    );
+  }
+
   bool isValid() => value.isRight();
 
   @override
---
+++ lib/domain/notes/todo_item.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/core/failures.dart';
+import 'package:ddd_notes/domain/core/value_objects.dart';
+import 'package:ddd_notes/domain/notes/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo_item.freezed.dart';
+
+@freezed
+class TodoItem with _$TodoItem {
+  const factory TodoItem({
+    required UniqueId id,
+    required TodoName name,
+    required bool done,
+  }) = _TodoItem;
+
+  factory TodoItem.empty() => TodoItem(
+        id: UniqueId(),
+        name: TodoName(''),
+        done: false,
+      );
+
+  const TodoItem._();
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return name.value.fold(
+      (l) => some(l),
+      (_) => none(),
+    );
+  }
+}
