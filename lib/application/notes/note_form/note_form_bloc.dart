71495d249d6b2150985d39307910cee82741bd3a
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-05-05 18:45:34 +0700

    Note Form Bloc

---
+++ lib/application/notes/note_form/note_form_bloc.dart
@@ -0,0 +1,75 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/notes/i_note_repository.dart';
+import 'package:ddd_notes/domain/notes/note.dart';
+import 'package:ddd_notes/domain/notes/note_failure.dart';
+import 'package:ddd_notes/domain/notes/value_objects.dart';
+import 'package:ddd_notes/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
+import 'dart:ui';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/collection.dart';
+
+part 'note_form_event.dart';
+part 'note_form_state.dart';
+part 'note_form_bloc.freezed.dart';
+
+@injectable
+class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
+  final INoteRepository _noteRepository;
+
+  NoteFormBloc(this._noteRepository) : super(NoteFormState.initial()) {
+    on<NoteFormEvent>((event, emit) async {
+      await event.map(
+        initialize: (e) async {
+          if (e.initialNoteOption != null) {
+            emit(state.copyWith(
+              note: e.initialNoteOption!,
+              isEditing: true,
+            ));
+          }
+        },
+        bodyChanged: (e) async {
+          emit(state.copyWith(
+            note: state.note.copyWith(body: NoteBody(e.bodyStr)),
+            saveFailureOrSuccessOption: none(),
+          ));
+        },
+        colorChanged: (e) async {
+          emit(state.copyWith(
+            note: state.note.copyWith(color: NoteColor(e.color)),
+            saveFailureOrSuccessOption: none(),
+          ));
+        },
+        todosChange: (e) async {
+          emit(state.copyWith(
+            note: state.note.copyWith(
+              todos: List3(e.todos.map((primitive) => primitive.toDomain())),
+            ),
+            saveFailureOrSuccessOption: none(),
+          ));
+        },
+        saved: (e) async {
+          late Either<NoteFailure, Unit> failureOrSuccess;
+
+          emit(state.copyWith(
+            isSaving: true,
+            saveFailureOrSuccessOption: none(),
+          ));
+
+          if (state.note.failureOption.isNone()) {
+            failureOrSuccess = state.isEditing
+                ? await _noteRepository.update(state.note)
+                : await _noteRepository.create(state.note);
+          }
+
+          emit(state.copyWith(
+            isSaving: false,
+            showErrorMessages: true,
+            saveFailureOrSuccessOption: optionOf(failureOrSuccess),
+          ));
+        },
+      );
+    });
+  }
+}
---
+++ lib/application/notes/note_form/note_form_event.dart
@@ -0,0 +1,11 @@
+part of 'note_form_bloc.dart';
+
+@freezed
+class NoteFormEvent with _$NoteFormEvent {
+  const factory NoteFormEvent.initialize(Note? initialNoteOption) = _Initialize;
+  const factory NoteFormEvent.bodyChanged(String bodyStr) = _BodyChanged;
+  const factory NoteFormEvent.colorChanged(Color color) = _ColorChanged;
+  const factory NoteFormEvent.todosChange(KtList<TodoItemPrimitive> todos) =
+      _TodosChange;
+  const factory NoteFormEvent.saved() = _Saved;
+}
